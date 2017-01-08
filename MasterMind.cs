52bb15b601f4fc857edb98cef421ca857ee1918a
fkingnoobgg <ghstoreinvesting@gmail.com>
2017-01-08 23:36:55 +0800

    Revert "add to do"
    This reverts commit cf7fd972e9f34ace1f48dde17452a3ec5e3e5ff9.

--- MasterMind.cs
+++ MasterMind.cs
@@ -13,10 +13,7 @@ using EloBuddy.SDK.Utils;
 using MasterMind.Components;
 using SharpDX;
 using Utility = MasterMind.Components.Utility;
-// TODO: 
-// show teemo passive and shrooms
-// show pink ward
-// show ward ranges
+
 namespace MasterMind
 {
     public static class MasterMind
